1e1e24141498a3293041ac91e37bf14ac5813440
carolinaoliveirasilveira <carolinaoliveirasilveira@outlook.com>
2024-12-05 15:16:46 -0300

    Primeiro commit

---
+++ src/test/java/project/carolina_oliveira/demo_rest_template/DemoRestTemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package project.carolina_oliveira.demo_rest_template;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoRestTemplateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
